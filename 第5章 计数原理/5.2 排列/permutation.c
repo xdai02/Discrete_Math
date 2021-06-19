739d5798c42dedabd3ecb6491ca47036907d79d1
xdai02 <2513276112@qq.com>
2021-06-19 22:46:44 +0800

    add chapter 5

---
+++ 第5章 计数原理/5.2 排列/permutation.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+void swap(char *a, char *b) {
+    char temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void permutation(char *s, int start, int end) {
+    if(start >= end) {
+        printf("%s\n", s);
+    } else {
+        for(int i = start; i < end; i++) {
+            swap(s + i, s + start);
+            permutation(s, start + 1, end);
+            swap(s + i, s + start);
+        }
+    }
+}
+
+int main() {
+    char s[] = "math";
+    int len = strlen(s);
+    permutation(s, 0, len);
+    return 0;
+}
