cbb576cdc5cd3ba9ebc4e90abda20442f5eef855
xdai02 <2513276112@qq.com>
2021-12-27 22:32:32 +0800

    add latex format

---
+++ clean.py
@@ -0,0 +1,20 @@
+import os
+
+EXCLUSION = [
+    ".aux",
+    ".bcf",
+    ".idx",
+    ".log",
+    ".ptc",
+    ".xml",
+    ".gz",
+    ".toc",
+    ".out"
+]
+
+for root, dirs, files in os.walk(os.getcwd()):
+    for file in files:
+        postfix = file[file.rfind("."):]
+        if postfix in EXCLUSION:
+            os.remove(os.path.join(root, file))
+            print("DELETED: " + file)
