0766f04e9c85c8388200aeb549377e8ffcedafa7
xdai02 <2513276112@qq.com>
2022-01-05 10:59:06 +0800

    format code style

--- clean.py
+++ clean.py
@@ -16,5 +16,8 @@ for root, dirs, files in os.walk(os.getcwd()):
     for file in files:
         postfix = file[file.rfind("."):]
         if postfix in EXCLUSION:
-            os.remove(os.path.join(root, file))
-            print("DELETED: " + file)
+            try:
+                os.remove(os.path.join(root, file))
+                print("DELETED: " + file)
+            except:
+                pass
